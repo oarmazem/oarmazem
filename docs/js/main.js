91c8b0bd23af21b19c387e39fbf3653960f0a806
oarmazem <cafereliquia@gmail.com>
2021-09-07 04:20:37 -0300

    Primeira versao responsiva do Index.html

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,17 +1,7 @@
-
 /*----------------------------------------------------------------------------
 
 ----------------------------------------------------------------------------*/
-function setIframeHeight() {
-  vh = window.innerHeight - (window.innerWidth * 9.4 / 100);
-  d = vh / 40;
-  document.getElementById("iframe").style.height = vh - d + 'px';
-}//setIframeHeight()
-
-/*----------------------------------------------------------------------------
-
-----------------------------------------------------------------------------*/
-currentOption = 'id1';
+var currentOption = 'id1';
 
 function nav(id) {
 
@@ -38,6 +28,16 @@ function nav(id) {
       break;
     case 'id5':
       menuOption.style.color = "#0FB99B";
+      break;
+    case 'id6':
+      menuOption.style.color = "#4F2C72";
+      break;    
+    case 'id7':
+      menuOption.style.color = "#1597D5";
+      break;  
+    case 'id8':
+      menuOption.style.color = "#A82F82";
+      break;        
   }//switch
   
 }//nav()
